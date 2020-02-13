c091cfb4dd30f14c2f0a6838c44380e1e6cdbc22
aram392 <arambalayan1@gmail.com>
2020-02-13 09:49:55 -0800

    added regex for staus checking.

---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"golang.org/x/net/html"
+
+	"github.com/gorilla/mux"
+)
+
+type License struct {
+	number      int
+	licenseType string
+	boardCode   string
+}
+
+func licenseRequest(w http.ResponseWriter, r *http.Request) {
+	//for testing with docker
+	fmt.Printf("got the request")
+
+	//TODO take user input and format it in x-www-form-urlencoded
+	//pass into dcaPost
+	createDcaPost()
+}
+
+//post to department of consumer affairs website
+func createDcaPost() {
+	url := "https://search.dca.ca.gov/results"
+	method := "POST"
+
+	//Hardcoded an example nurse. Change when passing in user input
+	//payload := strings.NewReader("boardCode=0&licenseType=0&firstName=RUBY&lastName=ABRANTES&licenseNumber=633681")
+	payload := strings.NewReader("boardCode=0&licenseType=0&firstName=RUBY&lastName=ABRANTES")
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//takes string of html. parses to nodes. (basically makes a tree of tags)
+	parseMe, err := html.Parse(strings.NewReader(string(body)))
+	htmlNodeTraversal(parseMe)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+//Finds tag we need and collects into text
+func htmlNodeTraversal(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "ul" {
+		for _, a := range n.Attr {
+			if a.Key == "class" && a.Val == "actions" {
+				text := &bytes.Buffer{}
+				collectionBuffer := collectText(n, text)
+				collectedText := collectionBuffer.String()
+				//fmt.Printf(collectedText)
+				verifyCollectedText(collectedText)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		htmlNodeTraversal(c)
+	}
+}
+
+//Go through the tree and write to buffer
+func collectText(n *html.Node, buf *bytes.Buffer) *bytes.Buffer {
+	if n.Type == html.TextNode {
+		buf.WriteString(n.Data)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		collectText(c, buf)
+	}
+	return buf
+}
+
+func verifyCollectedText(s string) {
+	//hardcoded we would need to change this for user specific data
+	name := "ABRANTES, RUBY"
+	number := "633681"
+	licenseType := "Registered Nurse"
+
+	expression := name + "+\\s+License Number:+\\s+" + number + "+\\s+License Type:+\\s+" + licenseType + "+\\s+License Status: Current"
+	//expression says return true if FirstName LastName + License Name and Type + License Status == Current.
+
+	var validID = regexp.MustCompile(expression)
+	fmt.Println("Match Result: " + name + " " + number + " " + licenseType + ": " + strconv.FormatBool(validID.MatchString(s)))
+}
+
+func main() {
+	fmt.Printf("Started Service\n")
+	//manually doing a post. for testing
+	createDcaPost()
+
+	//handler not setup
+	router := mux.NewRouter()
+	router.HandleFunc("/license", licenseRequest)
+	//log.Fatal(http.ListenAndServe(":8080", router))
+}
