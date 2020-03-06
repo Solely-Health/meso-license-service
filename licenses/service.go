605cf341531a5e4504a33537fae887251222c040
jazperez <jackkiey24@gmail.com>
2020-03-05 16:52:52 -0800

    Add updateLicense

--- licenses/service.go
+++ licenses/service.go
@@ -6,6 +6,7 @@ import (
 
 type Service interface {
 	StoreLicense(lic repository.License) (repository.License, error)
+	UpdateLicense(lic repository.License) error
 }
 
 type service struct {
@@ -15,6 +16,16 @@ type service struct {
 func (s *service) StoreLicense(lic repository.License) (repository.License, error) {
 	s.licenses.Store(&lic)
 	return lic, nil
+}
+
+func (s *service) UpdateLicense(lic repository.License) error {
+	// ObjectL := new(repository.License)
+	// ObjectL, err:= s.licenses.Find(lic.ID)
+	// if err != nil {
+
+	// }
+	// ObjectL = &lic
+	// return nil
 }
 
 func NewService(licenseRepository repository.LicenseRepository) Service {
